86eef2fca426c3d91ecf343cbebc9e4f5b0f59b1
Mikel Emaldi <m.emaldi@gmail.com>
2017-04-24 16:33:02 +0200

    Updated test for Building Block views

--- artifact_recommender/tests.py
+++ artifact_recommender/tests.py
@@ -1,76 +1,151 @@
-from django.test import TestCase
-from artifact_recommender.models import Dataset, BuildingBlock, Application
-from artifact_recommender.models import Idea, Similarity, Tag
+from django.test import TestCase, Client
+from artifact_recommender.models import Dataset, BuildingBlock, Tag
+from django.contrib.auth.models import User
+import base64
+import json
 # Create your tests here.
+
+BASIC_USER = 'test-user'
+BASIC_PASSWORD = 'test-password'
 
 
 class BuildingBlockTestCase(TestCase):
     def setUp(self):
-        self.tag1 = Tag.objects.create(name='tag1')
-        self.tag2 = Tag.objects.create(name='tag2')
-        self.tag3 = Tag.objects.create(name='tag3')
+        user = User.objects.create_user(BASIC_USER, password=BASIC_PASSWORD)
+        user.save()
+
+        self.client = Client()
+
+    def test_create_buildingblock_anon(self):
+        response = self.client.post('/buildingblock/',
+                                    json.dumps({'id': 4444,
+                                                'lang': 'spanish',
+                                                'tags': ['tag1', 'tag2']}),
+                                    content_type='application/json')
+
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(BuildingBlock.objects.count(), 0)
 
     def test_create_buildingblock(self):
-        three_tag_list = [self.tag1, self.tag2, self.tag3]
-        building_block = BuildingBlock.objects.create(id=4444, lang='spanish')
-        building_block.tags = three_tag_list
-        building_block.save()
+        response = self.client.post(
+            '/buildingblock/',
+            json.dumps({'id': 4444,
+                        'lang': 'spanish',
+                        'tags': ['tag1', 'tag2']}),
+            content_type='application/json',
+            **{'HTTP_AUTHORIZATION': 'BASIC {}'.format(
+                base64.b64encode('{}:{}'.format(
+                     BASIC_USER, BASIC_PASSWORD).encode()).decode())})
 
-        building_block = BuildingBlock.objects.last()
-
-        self.assertListEqual(list(building_block.tags.all()), three_tag_list)
-        self.assertEqual(building_block.lang, 'spanish')
-        self.assertEqual(building_block.id, 4444)
+        self.assertEqual(response.status_code, 201)
         self.assertEqual(BuildingBlock.objects.count(), 1)
 
-        two_tag_list = [self.tag1, self.tag2]
-        building_block = BuildingBlock.objects.create(id=5555, lang='spanish')
-        building_block.tags = two_tag_list
-        building_block.save()
+    def test_update_buildingblock_anon(self):
+        response = self.client.post(
+            '/buildingblock/',
+            json.dumps({'id': 4444,
+                        'lang': 'spanish',
+                        'tags': ['tag1', 'tag2']}),
+            content_type='application/json',
+            **{'HTTP_AUTHORIZATION': 'BASIC {}'.format(
+                base64.b64encode('{}:{}'.format(
+                     BASIC_USER, BASIC_PASSWORD).encode()).decode())})
 
-        building_block = BuildingBlock.objects.last()
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(BuildingBlock.objects.count(), 1)
 
-        self.assertListEqual(list(building_block.tags.all()), two_tag_list)
+        response = self.client.put(
+            '/buildingblock/',
+            json.dumps({'id': 4444,
+                        'lang': 'italian',
+                        'tags': ['tag3', 'tag4']}),
+            content_type='application/json')
+
+        self.assertEqual(response.status_code, 401)
+
+        building_block = BuildingBlock.objects.first()
         self.assertEqual(building_block.lang, 'spanish')
-        self.assertEqual(building_block.id, 5555)
-        self.assertEqual(BuildingBlock.objects.count(), 2)
+        tags = []
+        for tag in building_block.tags.all():
+            tags.append(tag.name)
+        self.assertListEqual(tags, ['tag1', 'tag2'])
 
     def test_update_buildingblock(self):
-        three_tag_list = [self.tag1, self.tag2, self.tag3]
-        two_tag_list = [self.tag1, self.tag2]
-        building_block = BuildingBlock.objects.create(id=4444, lang='spanish')
-        building_block.tags = three_tag_list
-        building_block.save()
+        response = self.client.post(
+            '/buildingblock/',
+            json.dumps({'id': 4444,
+                        'lang': 'spanish',
+                        'tags': ['tag1', 'tag2']}),
+            content_type='application/json',
+            **{'HTTP_AUTHORIZATION': 'BASIC {}'.format(
+                base64.b64encode('{}:{}'.format(
+                     BASIC_USER, BASIC_PASSWORD).encode()).decode())},
+            follow=True)
 
-        building_block = BuildingBlock.objects.last()
-
-        self.assertListEqual(list(building_block.tags.all()), three_tag_list)
-        self.assertEqual(building_block.lang, 'spanish')
-        self.assertEqual(building_block.id, 4444)
+        self.assertEqual(response.status_code, 201)
         self.assertEqual(BuildingBlock.objects.count(), 1)
 
-        building_block.lang = 'italian'
-        building_block.tags = two_tag_list
-        building_block.save()
+        response = self.client.put(
+            '/buildingblock/4444/',
+            json.dumps({'id': 4444,
+                        'lang': 'italian',
+                        'tags': ['tag3', 'tag4']}),
+            content_type='application/json',
+            **{'HTTP_AUTHORIZATION': 'BASIC {}'.format(
+                base64.b64encode('{}:{}'.format(
+                     BASIC_USER, BASIC_PASSWORD).encode()).decode())})
 
-        building_block = BuildingBlock.objects.last()
+        self.assertEqual(response.status_code, 200)
 
-        self.assertListEqual(list(building_block.tags.all()), two_tag_list)
+        building_block = BuildingBlock.objects.first()
         self.assertEqual(building_block.lang, 'italian')
-        self.assertEqual(building_block.id, 4444)
+        tags = []
+        for tag in building_block.tags.all():
+            tags.append(tag.name)
+        self.assertListEqual(tags, ['tag3', 'tag4'])
+
+    def test_delete_buildingblock_anon(self):
+        response = self.client.post(
+            '/buildingblock/',
+            json.dumps({'id': 4444,
+                        'lang': 'spanish',
+                        'tags': ['tag1', 'tag2']}),
+            content_type='application/json',
+            **{'HTTP_AUTHORIZATION': 'BASIC {}'.format(
+                base64.b64encode('{}:{}'.format(
+                     BASIC_USER, BASIC_PASSWORD).encode()).decode())},
+            follow=True)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(BuildingBlock.objects.count(), 1)
+
+        response = self.client.delete('/buildingblock/4444/')
+
+        self.assertEqual(response.status_code, 401)
         self.assertEqual(BuildingBlock.objects.count(), 1)
 
     def test_delete_buildingblock(self):
-        three_tag_list = [self.tag1, self.tag2, self.tag3]
-        building_block = BuildingBlock.objects.create(id=4444, lang='spanish')
-        building_block.tags = three_tag_list
-        building_block.save()
+        response = self.client.post(
+            '/buildingblock/',
+            json.dumps({'id': 4444,
+                        'lang': 'spanish',
+                        'tags': ['tag1', 'tag2']}),
+            content_type='application/json',
+            **{'HTTP_AUTHORIZATION': 'BASIC {}'.format(
+                base64.b64encode('{}:{}'.format(
+                     BASIC_USER, BASIC_PASSWORD).encode()).decode())},
+            follow=True)
 
+        self.assertEqual(response.status_code, 201)
         self.assertEqual(BuildingBlock.objects.count(), 1)
 
-        building_block = BuildingBlock.objects.last()
-        building_block.delete()
+        response = self.client.delete(
+            '/buildingblock/4444/',
+            **{'HTTP_AUTHORIZATION': 'BASIC {}'.format(
+                base64.b64encode('{}:{}'.format(
+                     BASIC_USER, BASIC_PASSWORD).encode()).decode())})
 
+        self.assertEqual(response.status_code, 204)
         self.assertEqual(BuildingBlock.objects.count(), 0)
 
 
