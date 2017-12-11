735355795970460de30daf938d22948612d95b8f
Mikel Emaldi <m.emaldi@gmail.com>
2017-12-11 15:25:02 +0100

    Indexing tags by name

--- artifact_recommender/models.py
+++ artifact_recommender/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=100, null=False, blank=False)
+    name = models.CharField(max_length=100, null=False, blank=False,
+                            db_index=True)
 
     def __str__(self):
         return self.name
